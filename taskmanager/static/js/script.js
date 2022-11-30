643c420f5f4ac660d0ef8f37910403374c5df148
BobWritesCode <warwick_hart@hotmail.com>
2022-11-30 22:48:22 +0000

    Add date picker and formSelect

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    $('.datepicker').datepicker({
+      format: "dd mmm, yyyy",
+      i18n: {done: "Select"}
+    });
+    $('select').formSelect();
   });
